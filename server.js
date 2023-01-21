16af6881a33a5d18091780faaf265062455c8428
Shawky Elshalawy <85047354+shawkyelshalawy@users.noreply.github.com>
2023-01-21 18:28:27 +0200

    developed the tour controller logic and designed the tour model , with all filtering , pagination

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app');
 
 dotenv.config({ path: './.env' });
+const app = require('./app');
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
